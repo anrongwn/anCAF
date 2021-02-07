7616c68ac76e2dc7a83c14f02fa95fa18d5263f4
anrongwn <anrong_wn@126.com>
2021-02-07 17:09:04 +0800

    update an_tp1

---
+++ an_tp1/main.cpp
@@ -0,0 +1,21 @@
+
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <string_view>
+#include <thread>
+#include <utility>
+
+#include "caf/actor_ostream.hpp"
+#include "caf/actor_system.hpp"
+#include "caf/caf_main.hpp"
+#include "caf/event_based_actor.hpp"
+
+using namespace caf;
+
+
+int main(int argc, char *argv[]) {
+	
+	return 0;
+}
